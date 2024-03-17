bb288275a97e012299989608278631e7eed46270
Katherine Beltrán <90588943+kaiouo@users.noreply.github.com>
2024-03-16 23:15:49 -0600

    arreglos

--- foro2/src/foro2/Foro2.java
+++ foro2/src/foro2/Foro2.java
@@ -7,9 +7,146 @@ package foro2;
 
 public class Foro2 {
 
+    
+    public static List<String> Equipo = new ArrayList<>();
+    public static List<String> laptops = new ArrayList<>();
+    public static List<String> tablets = new ArrayList<>();
    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        while (true) {
+            String inputEquipo = JOptionPane.showInputDialog(
+                    "1. Registrar Equipo\n" +
+                    "2. Ver Equipo\n" +
+                    "3. Salir\n\n" +
+                    "Ingrese una Opcion:");
+
+            int opcion = Integer.parseInt(inputEquipo.trim());
+
+            switch (opcion) {
+                case 1:
+                    registrarEquipo();
+                    break;
+               case 2:
+                    mostrarDatos();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida");
+                    break;
+            }
+        }
+        
+        
     }
     
+    private static void registrarEquipo() {
+    String inputTipoEquipo = JOptionPane.showInputDialog(
+            "1. Desktops\n" +
+                    "2. Laptops\n" +
+                    "3. Tablets\n\n" +
+                    "Ingrese una Opcion:");
+
+    int tipoEquipo = Integer.parseInt(inputTipoEquipo.trim());
+    
+    
+    switch (tipoEquipo) {
+        case 1:
+            // Lógica para registrar Desktops
+            break;
+        case 2:
+            // Lógica para registrar Laptops
+            break;
+        case 3:
+                String fabricante;
+                String modelo;
+                String microprocesador;
+                String taPantalla;
+                String capacitiva;
+                String meNan;
+                String sistemaOp;
+                do {
+                   fabricante = JOptionPane.showInputDialog("Ingrese el nombre del fabricante: ");
+               } while (fabricante.isEmpty());
+
+               do {
+                   modelo = JOptionPane.showInputDialog("Ingrese el nombre modelo: ");
+               } while (modelo.isEmpty());
+
+               do {
+                   microprocesador = JOptionPane.showInputDialog("Ingrese el nombre microprocesador: ");
+               } while (microprocesador.isEmpty());
+               
+                do {
+                   taPantalla = JOptionPane.showInputDialog("Ingrese el tamaño de la Pantalla: ");
+               } while (taPantalla.isEmpty());
+
+               do {
+                   capacitiva = JOptionPane.showInputDialog("Ingrese si es capacitiva (Sí/No): ");
+               } while (capacitiva.isEmpty());
+
+               do {
+                   meNan = JOptionPane.showInputDialog("Ingrese el tamaño de memoria NAND: ");
+               } while (meNan.isEmpty());
+
+               do {
+                   sistemaOp = JOptionPane.showInputDialog("Ingrese el Sistema Operativo: ");
+               } while (sistemaOp.isEmpty());
+
+                Tablets tablet = new Tablets(fabricante, modelo, microprocesador, taPantalla, capacitiva, meNan, sistemaOp);
+                tablet.agregarData();
+                
+                break;
+        default:
+            JOptionPane.showMessageDialog(null, "Opción no válida");
+            break;
+    }
+   
+}
+    
+    private static void mostrarDatos(){
+       String inputTipoEquipo = JOptionPane.showInputDialog(
+                "1. Desktops\n" +
+                        "2. Laptops\n" +
+                        "3. Tablets\n\n" +
+                        "Ingrese una Opcion:");
+
+        int tipoEquipo = Integer.parseInt(inputTipoEquipo.trim());
+
+        switch (tipoEquipo) {
+            case 2:
+                if (Equipo.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "No hay Equipos Registrados.");
+                } else {
+                    StringBuilder equiposRegistrados = new StringBuilder();
+                    equiposRegistrados.append("Equipos Registrados:\n");
+
+                    for (String equipo : Equipo) {
+                        equiposRegistrados.append(equipo).append("\n");
+                    }
+
+                    JOptionPane.showMessageDialog(null, equiposRegistrados.toString());
+                }
+                break;
+                case 3:
+                if (tablets.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "No hay Equipos Registrados.");
+                } else {
+                    StringBuilder equiposRegistrados = new StringBuilder();
+                    equiposRegistrados.append("Equipos Registrados:\n");
+
+                    for (String equipo : tablets) {
+                        equiposRegistrados.append(equipo).append("\n");
+                    }
+
+                    JOptionPane.showMessageDialog(null, equiposRegistrados.toString());
+                }
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+                break;
+        }
+    }
 }
--- foro2/src/foro2/Tablets.java
+++ foro2/src/foro2/Tablets.java
@@ -1,6 +1,11 @@
 
 package foro2;
 
+import static foro2.Foro2.tablets;
+    import javax.swing.JOptionPane;
+    import java.util.ArrayList;
+    import java.util.List;
+
 
 public class Tablets extends Equipo{
     
@@ -9,7 +14,7 @@ public class Tablets extends Equipo{
     private String meNan;
     private String sistemaOp;
     
-    public Tablets(String fabricante, String modelo, String microprocesador) {
+    public Tablets(String fabricante, String modelo, String microprocesador, String taPantalla, String capacitiva, String meNan, String sistemaOp) {
         super(fabricante, modelo, microprocesador);
         
         this.taPantalla = taPantalla;
@@ -22,7 +27,24 @@ public class Tablets extends Equipo{
          String data = ("Fabricante: "+getFabricante()+"\nModelo: "+getModelo()+"Microprocesador: "+getMicroprocesador()
                  +"\nTamaño diagonal de pantalla: "+taPantalla+"\nCapacitiva: "+capacitiva+"\nTamaño de memoria NAND: "+meNan
                  +"\nSistema Operativo: "+sistemaOp+"\n\n");  
+         
         return data;
     }
     
-}
+    
+    public String agregarData(){
+
+        String equipoRegistrado = "Fabricante: " + getFabricante() + "\n" +
+                "Modelo: " + getModelo() + "\n" +
+                "Microprocesador: " + getMicroprocesador() + "\n" +
+                "Tamaño Pantalla: " + taPantalla + "\n" +
+                "Capacitiva: " + capacitiva + "\n" +
+                "Tamaño de memoria NAND: " + meNan + "\n" +
+                "Sistema Operativo: " + sistemaOp;
+
+        JOptionPane.showMessageDialog(null, equipoRegistrado);
+        tablets.add(equipoRegistrado);
+        return equipoRegistrado;
+    }
+    
+    }
